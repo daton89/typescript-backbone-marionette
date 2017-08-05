eafd21119cb37ae0b16061e3f1bb89499c60e1c9
daton89 <tonydangelo123@gmail.com>
2017-08-05 22:22:01 +0200

    add index with marionette app

---
+++ index.ts
@@ -0,0 +1,11 @@
+import { Marionette } from "backbone.marionette";
+
+class MarionetteApp extends Marionette.Application {
+    constructor() {
+        super();
+        this.on("initialize:after", this.initializeAfter);
+    }
+    initializeAfter() {
+        alert("initializeAfter called");
+    }
+}
